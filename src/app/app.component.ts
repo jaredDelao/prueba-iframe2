e64ccfd218c0e630a9941d8cf43fb90ac89e954f
Jared <jared@MacBook-Pro-de-Jared.local>
2020-02-18 19:19:20 -0600

    f1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  AfterViewInit,
+  ViewChild,
+  ElementRef,
+  OnInit
+} from "@angular/core";
+import { fromEvent } from "rxjs";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-  title = 'prueba2';
+export class AppComponent implements OnInit {
+  
+  msg = fromEvent<MessageEvent>(window, "message");
+
+  ngOnInit() {
+    this.getData();
+  }
+
+  getData() {
+    // Recibe mensaje
+    this.msg.subscribe(event => {
+      console.log(event.data);
+      localStorage.setItem('token', event.data.value)
+    });
+  }
 }
